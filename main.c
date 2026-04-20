2bdb6e739b64b4b9b24ad8db2a89a553e1013789
wireacad <wireacad@baesystems.com>
2026-04-20 13:15:46 +0100

    added main file

---
+++ main.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+#define WIDTH 20
+#define HEIGHT 20
+#define ARRAY_GRIDSIZE HEIGHT*(WIDTH+1)
+
+//globals:
+bool EXIT_FLAG = 0;
+int PLAYER_X = 10;
+int PLAYER_Y = 10;
+char GRID[ARRAY_GRIDSIZE];
+
+enum {
+	CF_BLOCK = 1 << 0,
+	CF_ENEMY = 1 << 1,
+	CF_ITEM  = 1 << 2
+};
+
+/// credit: https://stackoverflow.com/questions/421860/capture-characters-from-standard-input-without-waiting-for-enter-to-be-pressed
+char getch() {
+	char buf = 0;
+	struct termios old = {0};
+	if (tcgetattr(0, &old) < 0)
+		perror("tcsetattr()");
+	old.c_lflag &= ~ICANON;
+	old.c_lflag &= ~ECHO;
+	old.c_cc[VMIN] = 1;
+	old.c_cc[VTIME] = 0;
+	if (tcsetattr(0, TCSANOW, &old) < 0)
+		perror("tcsetattr ICANON");
+	if (read(0, &buf, 1) < 0)
+		perror ("read()");
+	old.c_lflag |= ICANON;
+	old.c_lflag |= ECHO;
+	if (tcsetattr(0, TCSADRAIN, &old) < 0)
+		perror("tcsetattr ~ICANON");
+	return (buf);
+}
+
+int coordtoindex(int x, int y)
+{
+	return y*(WIDTH+1)+x;
+}
+
+int printgrid(char grid[])
+{
+	char *tg=malloc(sizeof(char) * ARRAY_GRIDSIZE);
+	memcpy(tg, grid, sizeof(char) * ARRAY_GRIDSIZE);	
+	
+	//add player to grid:
+	int coord = coordtoindex(PLAYER_X,PLAYER_Y);
+	tg[coord] = '@';
+
+	printf(tg);
+
+	free(tg);
+	return 0;
+}
+
+/// modulo an integer with no negatives
+int modc(int in, int mod)
+{
+	int res = in % mod;
+	if(res < 0)
+		res = mod + res;
+
+	return res;
+}
+
+int setgridch(char *grid, char ch, int x, int y)
+{
+	int mx = modc(x, WIDTH);
+	int my = modc(y, HEIGHT);
+	grid[coordtoindex(mx,my)]=ch;
+	return 0;
+}
+
+char getgridch(char *grid, int x, int y)
+{
+	int mx = modc(x, WIDTH);
+	int my = modc(y, HEIGHT);
+	return grid[coordtoindex(mx,my)];
+}
+
+int initgrid(char *grid, int W, int H)
+{
+	int i = 0;
+	for(int y = 0; y < H; y++)
+	{
+		for(int x = 0; x < W; x++)
+		{
+			i = (y*(W+1))+x;
+			grid[i]='.';
+		}
+		i = (y*(W+1))+W;
+		grid[i]='\n';
+	}
+	setgridch(grid, '#', 2, 2);
+	return 0;
+}
+
+
+
+int collision(char *grid, int x, int y)
+{
+	char c = getgridch(grid, x, y);
+	int res = 0;
+
+	res |= (c == '#') ? CF_BLOCK : 0;
+	res |= (isupper(c)) ? CF_ENEMY : 0;
+}
+
+int process(char input)
+{
+	int dx=0;
+	int dy=0;
+	if(input == 'q')
+	{
+		EXIT_FLAG = 1;
+	}
+	if(input == 'h')
+		dx = -1;
+	else if(input == 'j')
+		dy = -1;
+	else if(input == 'k')
+		dy = 1;
+	else if(input == 'l')
+		dx = 1;
+	
+	int tpx = modc(PLAYER_X + dx, WIDTH);
+	int tpy = modc(PLAYER_Y + dy, HEIGHT);
+	
+	int colres = collision(GRID, tpx,tpy);
+	if (colres & CF_BLOCK)
+	{
+		
+	}
+	else
+	{
+		PLAYER_Y = tpy;
+		PLAYER_X = tpx;
+	}
+
+	return 0;
+}
+
+int main(void)
+{
+	char input;
+	initgrid(GRID, WIDTH, HEIGHT);
+
+	while(!EXIT_FLAG)
+	{
+		system("clear");
+
+		printgrid(GRID);
+		printf("You pressed: %c \n", input);
+		input = getch();
+		process(input);
+	}
+
+	system("clear");
+	printf("Game Over!\n");
+	return 0;
+}
